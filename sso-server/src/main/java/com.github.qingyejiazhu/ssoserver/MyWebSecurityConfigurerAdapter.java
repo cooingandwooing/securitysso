bd9d4302a5686c1cf6c6c6b3311db2f2efb3304d
qingyejiazhu <qingyejiazhu@163.com>
2019-06-28 19:21:40 +0800

    Initial commit

---
+++ sso-server/src/main/java/com.github.qingyejiazhu/ssoserver/MyWebSecurityConfigurerAdapter.java
@@ -0,0 +1,48 @@
+package com.github.qingyejiazhu.ssoserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * ${desc}
+ * @author zhuqiang
+ * @version 1.0.1 2018/8/7 16:02
+ * @date 2018/8/7 16:02
+ * @since 1.0
+ */
+
+@Configuration
+public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .httpBasic()
+                .formLogin()  // 更改为form表单登录
+                .and()
+                // 所有的请求都必须授权后才能访问
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+        ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+}
